92d72bdaf500f2ba8d90703ab4a2069b354ba043
Benjamin Lévêque <benjamin@leveque.me>
2013-07-19 15:38:04 +0200

    intégration de behat [wip]

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
