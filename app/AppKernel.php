12075be3cf7834dddbafc42c26eb25a2c63327f1
Florian Rey <florian.rey@elao.com>
2013-12-19 10:04:14 +0100

    Add ladybug bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,9 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
 
+            // Debug
+            $bundles[] = new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle();
+
             // Fixtures
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
